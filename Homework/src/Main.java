85ddb602baf3c04876810c086566d82909516481
Fazekas <fazekas.nick101@gmail.com>
2016-02-17 21:36:06 -0500

    The entire rock paper scissors program
    I know the if else statement is a bit bad coding i did it on purpose so
    that it gives some one to fix if they want or leave

--- Homework/src/Main.java
+++ Homework/src/Main.java
@@ -1,9 +1,45 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
-
+		Scanner userInput = new Scanner(System.in);
+		//gets the user input
+		System.out.println("Please enter either Rock, Paper, or Scissors");
+		//sets the user input
+		String playerInput = userInput.nextLine();
+		//defines a Random object r
+		Random r = new Random(3);
+		
+		String opponentInput = null;
+		int randomNum = r.nextInt();
+		//determines what the opponent picks
+		if (randomNum == 0)
+			opponentInput = "rock";
+		else if (randomNum == 1)
+			opponentInput = "paper";
+		else
+			opponentInput = "scissor";
+		
+		
+		if(playerInput.equalsIgnoreCase(opponentInput))
+			System.out.println("ITS A DRAW!");
+		else if(playerInput.equalsIgnoreCase("rock") && opponentInput.equalsIgnoreCase("paper"))
+			System.out.println("Opponent Wins!");
+		else if(playerInput.equalsIgnoreCase("rock") && opponentInput.equalsIgnoreCase("scissor"))
+			System.out.println("Player Wins!");
+		else if(playerInput.equalsIgnoreCase("paper") && opponentInput.equalsIgnoreCase("rock"))
+			System.out.println("Player Wins!");
+		else if(playerInput.equalsIgnoreCase("paper") && opponentInput.equalsIgnoreCase("scissor"))
+			System.out.println("Opponent Wins!");
+		else if(playerInput.equalsIgnoreCase("scissor") && opponentInput.equalsIgnoreCase("paper"))
+			System.out.println("Player Wins!");
+		else if(playerInput.equalsIgnoreCase("scissor") && opponentInput.equalsIgnoreCase("rock"))
+			System.out.println("Opponent Wins!");
+		else
+			System.out.println("You did not pick rock, paper, or scissor. Please try again");
+		
 	}
 
 }
