04343de394af2446bbc9558a709616c96e0ea4b2
beltkeith721 <keithbelt56@gmail.com>
2016-02-19 06:58:36 -0500

    Switch statement (Keith)
    I added a switch statement to the random numbers for the opponent
    player for more efficient use of code.

--- Homework/src/Main.java
+++ Homework/src/Main.java
@@ -4,42 +4,47 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner userInput = new Scanner(System.in);
-		//gets the user input
+		Scanner userInput = new Scanner(System.in);	
+		// gets the user input
 		System.out.println("Please enter either Rock, Paper, or Scissors");
-		//sets the user input
+		System.out.println("Enter quit to stop the game");
+		// sets the user input
 		String playerInput = userInput.nextLine();
-		//defines a Random object r
+		// defines a Random object r
 		Random r = new Random(3);
-		
+
 		String opponentInput = null;
 		int randomNum = r.nextInt();
-		//determines what the opponent picks
-		if (randomNum == 0)
+		//added switch statement for the random number
+		switch (randomNum) {
+		case 0:
 			opponentInput = "rock";
-		else if (randomNum == 1)
+			break;
+		case 1:
 			opponentInput = "paper";
-		else
+			break;
+		default:
 			opponentInput = "scissor";
+
+		}
 		
-		
-		if(playerInput.equalsIgnoreCase(opponentInput))
+		if (playerInput.equalsIgnoreCase(opponentInput))
 			System.out.println("ITS A DRAW!");
-		else if(playerInput.equalsIgnoreCase("rock") && opponentInput.equalsIgnoreCase("paper"))
+		else if (playerInput.equalsIgnoreCase("rock") && opponentInput.equalsIgnoreCase("paper"))
 			System.out.println("Opponent Wins!");
-		else if(playerInput.equalsIgnoreCase("rock") && opponentInput.equalsIgnoreCase("scissor"))
+		else if (playerInput.equalsIgnoreCase("rock") && opponentInput.equalsIgnoreCase("scissor"))
 			System.out.println("Player Wins!");
-		else if(playerInput.equalsIgnoreCase("paper") && opponentInput.equalsIgnoreCase("rock"))
+		else if (playerInput.equalsIgnoreCase("paper") && opponentInput.equalsIgnoreCase("rock"))
 			System.out.println("Player Wins!");
-		else if(playerInput.equalsIgnoreCase("paper") && opponentInput.equalsIgnoreCase("scissor"))
+		else if (playerInput.equalsIgnoreCase("paper") && opponentInput.equalsIgnoreCase("scissor"))
 			System.out.println("Opponent Wins!");
-		else if(playerInput.equalsIgnoreCase("scissor") && opponentInput.equalsIgnoreCase("paper"))
+		else if (playerInput.equalsIgnoreCase("scissor") && opponentInput.equalsIgnoreCase("paper"))
 			System.out.println("Player Wins!");
-		else if(playerInput.equalsIgnoreCase("scissor") && opponentInput.equalsIgnoreCase("rock"))
+		else if (playerInput.equalsIgnoreCase("scissor") && opponentInput.equalsIgnoreCase("rock"))
 			System.out.println("Opponent Wins!");
 		else
 			System.out.println("You did not pick rock, paper, or scissor. Please try again");
-		
+		}
+
 	}
 
-}
